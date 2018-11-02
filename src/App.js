cf7316c4f396ec6a8f9ff5993d65b79d2cb2d7c3
Frank Riccobono <F.Riccobono@gmail.com>
2018-11-01 22:47:20 -0400

    Initial commit - created with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import moment from "moment";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+type ProjectProps = {
+    isActive: boolean,
+    onClick: any,
+    name: string
+};
+
+function Project(props: ProjectProps) {
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      if (props.isActive) {
+        setDuration(duration + 1000);
+      }
+    }, 1000);
+    return function() {
+      clearTimeout(timeout);
+    };
+  });
+
+  return (
+    <p onClick={props.onClick} className={props.isActive ? "active" : ""}>
+      {props.name} : {moment.utc(duration).format("HH:mm:ss")}{" "}
+    </p>
+  );
+}
+
+function App() {
+  const [project, setProject] = useState("");
+  const [projects, setProjects] = useState(["Other"]);
+  const [activeProject, setActiveProject] = useState(0);
+
+  function handleProjectChange(evt) {
+    setProject(evt.target.value);
   }
+
+  function handleAddProject() {
+    setProjects([...projects, project]);
+  }
+
+  function handleSelectProject(i) {
+    return function() {
+      setActiveProject(i);
+    };
+  }
+
+  return (
+    <div className="App">
+      <input name="project" value={project} onChange={handleProjectChange} />
+      <button onClick={handleAddProject}>Add</button>
+      {projects.map((p, i) => (
+        <Project
+          onClick={handleSelectProject(i)}
+          isActive={i === activeProject}
+          key={i}
+          name={p}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
